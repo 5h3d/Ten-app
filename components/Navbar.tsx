e3dbfacb69e3f07a9eb07eef7b61e2180ff05413
Udeh Shedrack <78221827+5h3d@users.noreply.github.com>
2022-12-15 07:48:06 +0100

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -34,12 +34,9 @@ const Navbar = () => {
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href='/'>
         <div className='w-[100px] md:w-[129px] md:h-[30px] h-[38px]'>
-          <Image
-            className='cursor-pointer'
-            src={Logo}
-            alt='logo'
-            layout='responsive'
-          />
+         <h1 className="logo font-bold uppercase text-3xl text-[#F51997]">
+            TenApp
+          </h1>
         </div>
       </Link>
 
